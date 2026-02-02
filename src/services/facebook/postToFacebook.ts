92ffe79cb663da7005a88f9831fc6aa845ea7afb
robin <hossainmdrobin967@gmail.com>
2026-02-03 01:54:07 +0600

    Id is made dynamic

--- src/services/facebook/postToFacebook.ts
+++ src/services/facebook/postToFacebook.ts
@@ -12,41 +12,6 @@ interface Account extends Document {
     socialId: string;
 }
 
-// FUNCTION TO UPLOAD VIDEO
-const postWithVedio = async ({ fbUrl, post }: { fbUrl: string, post: any }) => {
-    const res = await axios.post(
-        fbUrl,
-        {
-            postage_url: post.videos[0],
-            uploaded: true,
-            // published: false,
-            access_token: post.account.token,
-        }
-    );
-    console.log("Video uploaded");
-
-    const url = "https://graph.facebook.com/v19.0/me/feed";
-    const response = await axios.post(url, null, {
-        params: {
-            message: post.text,
-            attached_media: res.data.id,
-            access_token: post?.account?.token, // Ensure you have the correct access token
-        },
-    });
-    console.log("Post created with video");
-    post.stage = "published";
-    post.publishedAt = new Date();
-    post.socialId = response.data.id;
-    const notification = new Notificaiton({
-        admin: post.admin,
-        title: "New Post Published",
-        message: `A new post has been published to Facebook with ID: ${response.data.id}`,
-    })
-    await notification.save();
-    await post.save();
-
-}
-
 // POSTING VIDEOS INCLUDING iMAGE OR VIDEO
 export const postToFacebook = async (postId: string) => {
     if (!postId) return;
@@ -57,12 +22,12 @@ export const postToFacebook = async (postId: string) => {
     try {
         // UPLOADING VIDEO IF EXISTS
         if (post.videos && post.videos.length > 0) {
-            const res = await axios.post(`https://graph.facebook.com/v17.0/761979666988561/videos`, {
+            const res = await axios.post(`https://graph.facebook.com/v17.0/${post.account.socialId}/videos`, {
                 file_url: post.videos[0],
                 access_token: post.account.token,
                 description: post.text
             });
-            console.log("Video upload response:", res.data);
+            console.log("Video upload response:", res);
 
             post.stage = "published";
             post.publishedAt = new Date();
