9c547c114ce3d7af378d6aa3b592d9aab737f5d8
robin <hossainmdrobin967@gmail.com>
2026-04-01 02:18:39 +0600

    Go for institute

--- src/controllers/productController/product.controller.ts
+++ src/controllers/productController/product.controller.ts
@@ -10,7 +10,7 @@ export const createProduct = async (req: Request, res: Response) => {
     else images.push(file.path);
   });
   try {
-    const product = new Product({...req.body, images,videos,uploadedBy:req.user._id});
+    const product = new Product({...req.body, images,videos,uploadedBy:req.user._id, institute:req.user.institute});
     const savedProduct = await product.save();
     res.status(201).json({ success: true, data: savedProduct });
   } catch (err) {
