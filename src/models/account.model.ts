80c5d05c1863d9eaf30b50d200f8ffd37e163464
robin <hossainmdrobin967@gmail.com>
2026-04-14 00:00:12 +0600

    default

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const accountSchema = new mongoose.Schema({
-  institute:{ type: mongoose.Schema.Types.ObjectId, ref: "Institute", required: true },
-  socialId: { type: String, required: true }, 
+  institute: { type: mongoose.Schema.Types.ObjectId, ref: "Institute", required: true },
+  socialId: { type: String, required: true },
   name: { type: String, required: true },
   photo: { type: String, required: false },
   token: { type: String, required: true },
-  platform: { type: String, required: true, enum: ["facebook", "x", "youtube", "tiktok", "pinterest"] },
+  platform: { type: String, default: "facebook", required: true, enum: ["facebook", "x", "youtube", "tiktok", "pinterest"] },
   accountType: { type: String, required: true, enum: ["page", "profile"] },
   tokenExpires: { type: Date, required: false },
   addedBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true }, // generated from jwt token
--- src/routes/accounts/accouts.routes.ts
+++ src/routes/accounts/accouts.routes.ts
@@ -4,8 +4,6 @@ import { authenticateToken } from "../../middleware/auth.middleware";
 
 const router = express.Router();
 
-// router.post("/send-otp", (req, res) => {});
-// router.post("change-password", (req, res) => {});
 router.post("/add-account",authenticateToken, addAccount);
 router.delete("/remove-account/:id", authenticateToken, removeAccount );
 router.get("/get-account",authenticateToken,getAccountsByInstitute)
